a81428788c26ccf1e2f55743ed261479111f0fc2
Christian Spence <mail@christianspence.co.uk>
2022-10-28 16:01:09 +0100

    Now loads RDS based on folder location rather than individual files

--- R/data-load-from-rds.R
+++ R/data-load-from-rds.R
@@ -1,29 +1,8 @@
-# fedo_dict    <- readr::read_csv("data-raw/fedo_dict.csv")
+dataFileLocation <- "data/datasets"
 
-#ons_datasets <- readr::read_rds("data/ons_datasets.rds")
+dataFiles <- list.files(dataFileLocation, full.names = TRUE)
 
-#variables    <- readr::read_rds("data/variables.rds")
-#boundaries   <- readr::read_rds("data/boundaries_nuts1-2-3.rds")
-RGVA         <- readr::read_rds("data/datasets.knaresborough/rgva_msoa.rds")
-#RGVAI        <- readr::read_rds("data/ons_rgvai.rds")
-BRES         <- readr::read_rds("data/datasets.knaresborough/bres_msoa.rds")
-UKBC         <- readr::read_rds("data/datasets.knaresborough/ukbc_knaresborough.rds")
-#RGFCF        <- readr::read_rds("data/RGFCF.rds")
-CTSOP        <- readr::read_rds("data/datasets.knaresborough/ctsop_msoa_knaresborough.rds")
-SAIE         <- readr::read_rds("data/datasets.knaresborough/saie.rds")
-
-sp_datasets  <- list(RGVA  = RGVA,
-                     #RGVAI = RGVAI,
-                     BRES  = BRES,
-                     UKBC  = UKBC,
-                     #RGFCF = RGFCF,
-                     CTSOP = CTSOP,
-                     SAIE = SAIE)
-
-datasets <- list(# ons_datasets = ons_datasets,
-                 sp_datasets = sp_datasets)
-
-# this added to provide a second option - instead of two lists within a list
-# we provide a single all_datasets list which contains ons and spatial
-eddie_datasets <- c(# ons_datasets,
-                    sp_datasets)
+eddie_datasets <- lapply(dataFiles, function(file) {
+  readRDS(file)
+}) |>
+  setNames(tools::file_path_sans_ext(basename(dataFiles)))
