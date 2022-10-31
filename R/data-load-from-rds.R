c7928a2d41061bd232d613a3c62b3bca73afccbe
Christian Spence <mail@christianspence.co.uk>
2022-10-31 15:17:25 +0000

    Create new dynamic object loading

--- R/data-load-from-rds.R
+++ R/data-load-from-rds.R
@@ -1,29 +1,39 @@
-# fedo_dict    <- readr::read_csv("data-raw/fedo_dict.csv")
+dataFileLocation <- "data/datasets"
 
-#ons_datasets <- readr::read_rds("data/ons_datasets.rds")
+dataFiles <- list.files(dataFileLocation, full.names = TRUE)
 
-#variables    <- readr::read_rds("data/variables.rds")
-#boundaries   <- readr::read_rds("data/boundaries_nuts1-2-3.rds")
-RGVA         <- readr::read_rds("data/datasets.knaresborough/RGVA.rds")
-#RGVAI        <- readr::read_rds("data/ons_rgvai.rds")
-BRES         <- readr::read_rds("data/datasets.knaresborough/BRES.rds")
-UKBC         <- readr::read_rds("data/datasets.knaresborough/UKBC.rds")
-#RGFCF        <- readr::read_rds("data/RGFCF.rds")
-CTSOP        <- readr::read_rds("data/datasets.knaresborough/CTSOP.rds")
-SAIE         <- readr::read_rds("data/datasets.knaresborough/SAIE.rds")
+eddie_datasets <- lapply(dataFiles, \(x) readRDS(x)) |>
+  setNames(tools::file_path_sans_ext(basename(dataFiles)))
 
-sp_datasets  <- list(RGVA  = RGVA,
-                     #RGVAI = RGVAI,
-                     BRES  = BRES,
-                     UKBC  = UKBC,
-                     #RGFCF = RGFCF,
-                     CTSOP = CTSOP,
-                     SAIE = SAIE)
 
-datasets <- list(# ons_datasets = ons_datasets,
-                 sp_datasets = sp_datasets)
+################################################################
 
-# this added to provide a second option - instead of two lists within a list
-# we provide a single all_datasets list which contains ons and spatial
-eddie_datasets <- c(# ons_datasets,
-                    sp_datasets)
+# # fedo_dict    <- readr::read_csv("data-raw/fedo_dict.csv")
+#
+# #ons_datasets <- readr::read_rds("data/ons_datasets.rds")
+#
+# #variables    <- readr::read_rds("data/variables.rds")
+# #boundaries   <- readr::read_rds("data/boundaries_nuts1-2-3.rds")
+# RGVA         <- readr::read_rds("data/datasets.knaresborough/RGVA.rds")
+# #RGVAI        <- readr::read_rds("data/ons_rgvai.rds")
+# BRES         <- readr::read_rds("data/datasets.knaresborough/BRES.rds")
+# UKBC         <- readr::read_rds("data/datasets.knaresborough/UKBC.rds")
+# #RGFCF        <- readr::read_rds("data/RGFCF.rds")
+# CTSOP        <- readr::read_rds("data/datasets.knaresborough/CTSOP.rds")
+# SAIE         <- readr::read_rds("data/datasets.knaresborough/SAIE.rds")
+#
+# sp_datasets  <- list(RGVA  = RGVA,
+#                      #RGVAI = RGVAI,
+#                      BRES  = BRES,
+#                      UKBC  = UKBC,
+#                      #RGFCF = RGFCF,
+#                      CTSOP = CTSOP,
+#                      SAIE = SAIE)
+#
+# datasets <- list(# ons_datasets = ons_datasets,
+#   sp_datasets = sp_datasets)
+#
+# # this added to provide a second option - instead of two lists within a list
+# # we provide a single all_datasets list which contains ons and spatial
+# eddie_datasets <- c(# ons_datasets,
+#   sp_datasets)
