13f5a71b875e7d1ee01e16c35e57026b8d715b6f
Christian Spence <mail@christianspence.co.uk>
2022-10-31 10:06:27 +0000

    UI text renaming and clean up

--- R/ui.R
+++ R/ui.R
@@ -1,14 +1,14 @@
 ui <- navbarPage(
-  title = "EDDIE v0.0.0.9000 ALPHA",
+  title = "EDD v0.0.0.9000 ALPHA",
   id = "navbar",
-  windowTitle = "EDDIE: Economic Data Dashboard with Integrated Exploration",
+  windowTitle = "EDD: Economic Data Dashboard",
 
   tabPanel(
     id = "datatool",
     title = "Interactive data tool",
 
     # Application title
-    titlePanel("Knaresborough Economic Data Dashboard"),
+    titlePanel("Economic Data Dashboard"),
 
     # Sidebar UI
     sidebarLayout(
@@ -63,13 +63,13 @@ ui <- navbarPage(
   tabPanel(
     id = "about",
     title = "About",
-    h2("About EDDIE"),
-    p("EDDIE, the Economic Data Dashboard with Integrated Exploration, is a tool currently under development."),
+    h2("About EDD"),
+    p("EDD, the Economic Data Dashboard, is a tool currently under development."),
     p("It is designed to make economic data more useful, particularly at a sub-regional level, to support better analysis and policymaking in local areas."),
-    p("EDDIE will, in its first beta release (v0.0.1 expected Q4 2022), provide interactive access to most ONS datasets, including small area estimates, providing a repository of small-geography economic data in a structured, linked format, with ISO8601 date formats and standard ONS geography codes to support easier access to cleaned and cubed data from multiple sources."),
-    p("Later, EDDIE will add Land Registry housing transacation data, all NOMIS datasets and Companies House data for all active companies in the UK, including an API to access all data across multiple sources."),
+    p("EDD will, in its first beta release (v0.0.1 expected Q4 2022), provide interactive access to most ONS datasets, including small area estimates, providing a repository of small-geography economic data in a structured, linked format, with ISO8601 date formats and standard ONS geography codes to support easier access to cleaned and cubed data from multiple sources."),
+    p("Later, EDD will add Land Registry housing transacation data, all NOMIS datasets and Companies House data for all active companies in the UK, including an API to access all data across multiple sources."),
     p("The development plan includes both imputation of all economic data to a small-area level (not less than MSOA) and a quarterly-updated forecasting model for all economies across the UK. Both of these will be on a commercial subscription basis."),
-    p("If you're interested in its development, more information is available on its Github page at github.com/ChristianSpence/eddie."),
+    p("If you're interested in its development, more information is available on its Github page at github.com/ChristianSpence/edd."),
     p("For more information, please contact Christian Spence at mail@christianspence.co.uk")
   ),
 
