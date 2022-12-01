7f30a173efda244e166aa7926ac260f7d804734b
Christian Spence <mail@christianspence.co.uk>
2022-12-01 16:10:51 +0000

    Improve functionality to convert to edd_obj

--- R/data-ons-rgdhi-lad.R
+++ R/data-ons-rgdhi-lad.R
@@ -8,24 +8,29 @@ rgdhi_lad <- function() {
   files <- urls[grepl(".xls", urls)]
   file_urls <- paste0("http://ons.gov.uk", files) # http not https
 
-  all_data <- lapply(file_urls, function(file) {
-    local_file_path <- paste0("~/Data/ONS/Regional Accounts/GDHI/", basename(file))
+  all_data <- lapply(file_urls[1:12], function(file) {
+    local_file_path <- paste0("~/Data/ONS/Regional Accounts/GDHI/",
+                              sub("regionalgrossdisposablehouseholdincome",
+                                  "rgdhi", basename(substring(file, 50))))
     download.file(file, destfile = local_file_path, mode = "wb")
     sheets <- readxl::excel_sheets(local_file_path)
     sheets <- sheets[-(1:2)]
     data <- lapply(sheets, function(sht) {
-      readxl::read_excel(local_file_path, sheet = sht, skip = 1, na = c("-"))
-    })
-    names(data) <- c("GDHI current",
-                     "GDHI per capita current",
-                     "GDHI per capita index",
-                     "GDHI annual growth",
-                     "GDHI per capita annual growth",
-                     "GDHI components current",
-                     "GDHI per capita components",
-                     "GDHI per capita components index",
-                     "GDHI components growth",
-                     "GDHI per capita components growth")
+      readxl::read_excel(local_file_path, sheet = sht,
+                         skip = 1, na = c("-"))
+    }) |>
+      setNames(c("GDHI current",
+                 "GDHI per capita current",
+                 "GDHI per capita index",
+                 "GDHI annual growth",
+                 "GDHI per capita annual growth",
+                 "GDHI components current",
+                 "GDHI per capita components",
+                 "GDHI per capita components index",
+                 "GDHI components growth",
+                 "GDHI per capita components growth")
+      )
+
     return(data)
   })
 
@@ -38,10 +43,26 @@ rgdhi_lad <- function() {
       }
 
       dplyr::filter(sht, !is.na(`LAD code`)) |>
-        tidyr::pivot_longer(cols = -(c), names_to = "date") |>
-        dplyr::mutate(date = paste0(stringr::str_sub(date, 1, 4), "-01-01") |> as.Date())
+        tidyr::pivot_longer(cols = -(c), names_to = "date")
     }) |>
       dplyr::bind_rows(.id = "variable")
   }) |>
-    dplyr::bind_rows()
+    dplyr::bind_rows() |>
+    dplyr::select(date,
+                  geography_code = `LAD code`,
+                  geography_name = `Region name`,
+                  variable_code = variable,
+                  variable_name = variable,
+                  transaction_code = `Transaction code`,
+                  transaction_name = Transaction,
+                  value) |>
+    dplyr::mutate(date = stringr::str_sub(date, 1, 4)) |>
+    tidyr::replace_na(list(transaction_name = "Total")) |>
+    dplyr::mutate(transaction_code = dplyr::coalesce(transaction_code,
+                                                     transaction_name)) |>
+    df_to_edd_df() |>
+    edd_df_to_edd_obj()
+
+    saveRDS(final, "data/datasets/RGDHI.rds")
+    return(final)
 }
