a140f4981d5a24271712d952dc94f4f003da3839
Christian Spence <mail@christianspence.co.uk>
2023-09-07 13:54:44 +0100

    Rewrite of date_text_to_df for speed improvements (x200!)

--- R/utils-date-formats.R
+++ R/utils-date-formats.R
@@ -89,10 +89,69 @@ date_text_to_iso <- function(date_as_text) {
   return(out)
 }
 
+# date_text_to_df <- function(dates) {
+#   message("Parsing date formats (can be slow) ...")
+#   # if the date is four digits only, i.e. a year and annual
+#   out_df <- purrr::map_df(dates, function(d) {
+#     if (grepl("^[0-9]{4}$", d)) {
+#       date <- readr::parse_date(d, format = "%Y")
+#       freq <- "a"
+#     } else if (grepl("Q", d)) { # if the date contains a "Q", i.e. quarterly
+#       date <- lubridate::yq(d)
+#       freq <- "q"
+#     } else if (grepl("[0-9]{2}-[0-9]{2}-[0-9]{4}", d)) { # if the date is DD-MM-YYYY
+#       date <- as.Date(d, format = "%d-%m-%Y")
+#       freq <- "d"
+#     } else if (grepl("[0-9]{4}$", d)) { # if contains four digits at the end
+#       date <- lubridate::my(d)
+#       freq <- "m"
+#     } else if (grepl("^[0-9]{4}", d)) { # if it contains four digits at the beginning
+#       date <- lubridate::ymd(d, truncated = 1)
+#       freq <- "m"
+#     } else {
+#       date <- as.Date(NA) # if none of the above parse, return an NA to highlight the problem
+#       freq <- NA_character_
+#     }
+#     df <-  tibble::tibble(date = date,
+#                           freq = freq)
+#   })
+#   message("Done.")
+#   return(out_df)
+# }
+
+# NEW VERSION OF THE ABOVE
+
 date_text_to_df <- function(dates) {
-  message("Parsing date formats (can be slow) ...")
-  # if the date is four digits only, i.e. a year and annual
-  out_df <- purrr::map_df(dates, function(d) {
+
+  #stopifnot(is.vector(dates), "`dates` is not a vector")
+
+  if (class(dates) == "Date") {
+    stop("`dates` already has `class()` of `Date`. Cannot impute frequency")
+  }
+
+  if (!is.character(dates)) {
+    dates2 <- as.character(dates)
+    if (all(dates == dates2)) {
+      dates <- dates2
+    } else {
+      stop("`dates` cannot be coerced to a character vector")
+    }
+  }
+
+  message("Parsing date formats ...")
+
+  n <- length(dates)
+  dates_df <- data.frame(original_date = dates)
+  dates_df$order <- 1:n
+  dates_df <- dates_df |>
+    dplyr::arrange(original_date)
+
+  # prepare output vectors
+  date <- vector(length = n)
+  freq <- character(length = n)
+
+  parse_dates <- function(d) {
+    # if the date is four digits only, i.e. a year and annual
     if (grepl("^[0-9]{4}$", d)) {
       date <- readr::parse_date(d, format = "%Y")
       freq <- "a"
@@ -114,10 +173,39 @@ date_text_to_df <- function(dates) {
     }
     df <-  tibble::tibble(date = date,
                           freq = freq)
-  })
+    return(df)
+  }
+
+  # iterate through `dates`
+  previous_date <- ""
+  for (d in 1:n) {
+
+    if (previous_date != dates_df$original_date[d]) {
+      temp_df <- parse_dates(dates_df$original_date[d])
+    }
+
+    date[d] <- temp_df$date
+    freq[d] <- temp_df$freq
+    previous_date <- dates_df$original_date[d]
+  }
+
+
+
+  dates_df$date <- as.Date(date)
+  dates_df$freq <- freq
+
+  dates_df <- dates_df |>
+    dplyr::arrange(order) |>
+    dplyr::select(date, freq) |>
+    tibble::as_tibble()
+
+
   message("Done.")
-  return(out_df)
+  return(dates_df)
+
 }
+
+
 
 # DEPRECATED
 
