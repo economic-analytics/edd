f460f511d64eb90ae430d754fc8b5580b9832a81
Christian Spence <mail@christianspence.co.uk>
2024-01-23 15:01:43 +0000

    Update edd_obj_to_dataframe to R pipe

--- R/data-edd_obj-to-dataframe.R
+++ R/data-edd_obj-to-dataframe.R
@@ -1,18 +1,19 @@
 edd_obj_to_dataframe <- function(edd_obj) {
-  # identify all dimensions in the df that's been passed in
+  # identify all dimensions in the edd_obj that's been passed in
   dims <- names(edd_obj$dimensions)
   data <- edd_obj$data
   for (d in seq_along(dims)) {
     dimension <- edd_obj[["dimensions"]][[d]]
     data <- dplyr::left_join(
       data,
-      dimension %>% dplyr::select(name, code), # the select restriction is in place until we can dynamically handle $unit, $type, etc.
+      dimension |>
+        dplyr::select(name, code), # the select restriction is in place until we can dynamically handle $unit, $type, etc.
       by = setNames("code", dims[d]) # evaluates to: by = c("dims[d] = "code")
-    ) %>%
+    )  |>
       # at this point it handles dim$name and dim$code, but also needs to manage $unit, $type etc. when present
       dplyr::mutate(!!dims[d] := tibble::tibble(code = !!data[[dims[d]]],
                                                 name = name)
-      ) %>%
+      ) |>
       dplyr::select(-name)
   }
   return(data)
