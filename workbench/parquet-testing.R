a1e92e805566c2e721d0f388338591a1b948a35d
Christian Spence <mail@christianspence.co.uk>
2024-02-05 11:46:02 +0000

    Create single parquet directory

--- workbench/parquet-testing.R
+++ workbench/parquet-testing.R
@@ -1,20 +1,59 @@
+dir.create("workbench/parquet")
+
 edd_datasets$UNEM |>
   edd_obj_to_dataframe() |> 
-  tidyr::unnest(names_sep = "_") |>
-  arrow::write_parquet("workbench/unem.parquet")
+  tidyr::unnest(names_sep = ".") |>
+  arrow::write_parquet("workbench/parquet/unem.parquet")
 
-unem <- arrow::read_parquet("workbench/unem.parquet",  as_data_frame = FALSE)
+edd_datasets$MM23 |>
+  edd_obj_to_dataframe() |> 
+  tidyr::unnest(names_sep = ".") |>
+  arrow::write_parquet("workbench/parquet/mm23.parquet")
+
+edd_datasets$UNEM |>
+  edd_obj_to_dataframe() |> 
+  tidyr::unnest(names_sep = ".") |>
+  readr::write_csv("workbench/unem.csv")
+
+
+unem <- arrow::read_parquet("workbench/unem.parquet",  as_data_frame = T)
 
 str(unem)
 
 unem <- arrow::read_parquet(
   "https://github.com/economic-analytics/edd/raw/main/workbench/unem.parquet",
-  as_data_frame = T
+  as_data_frame = FALSE
 )
 
-unem$variable_name |> unique()
+mm23 <- arrow::read_parquet(
+  "https://github.com/economic-analytics/edd/raw/main/workbench/mm23.parquet",
+  as_data_frame = FALSE
+)
+
+u |>nem$variable.name |> dplyr::collect()
 
 
 unem |>
-  dplyr::distinct(variable_name) |>
+  dplyr::distinct(variable.name) |>
   dplyr::collect()
+
+unem |> dplyr::collect()
+
+mm23  |>
+  dplyr::collect()
+
+d <- c("unem", "mm23")
+
+lapply(seq_along(d), function(x) {
+  get(d[x]) |>
+  dplyr::collect()
+  }) |> 
+  setNames(d) |>
+  dplyr::bind_rows(.id = "dataset")
+
+pd <- arrow::open_dataset("workbench/parquet")
+arrow::open_dataset()
+pd$variable.name
+
+pd |>
+dplyr::distinct(variable.name) |> dplyr::collect()
