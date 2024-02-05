a037d5e7e455c82ac552f01080e56542579b9459
Christian Spence <mail@christianspence.co.uk>
2024-02-05 11:03:41 +0000

    Add testing for github remote parquet

--- workbench/parquet-testing.R
+++ workbench/parquet-testing.R
@@ -1,6 +1,6 @@
 edd_datasets$UNEM |>
   edd_obj_to_dataframe() |> 
-  tidyr::unnest() |>
+  tidyr::unnest(names_sep = "_") |>
   arrow::write_parquet("workbench/unem.parquet")
 
 unem <- arrow::read_parquet("workbench/unem.parquet",  as_data_frame = FALSE)
@@ -9,7 +9,12 @@ str(unem)
 
 unem <- arrow::read_parquet(
   "https://github.com/economic-analytics/edd/raw/main/workbench/unem.parquet",
-  as_data_frame = FALSE
+  as_data_frame = T
 )
 
-unem$variable
+unem$variable_name |> unique()
+
+
+unem |>
+  dplyr::distinct(variable_name) |>
+  dplyr::collect()
