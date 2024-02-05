bafb485e07963b1c151b86c756d37d94ebad82f7
Christian Spence <mail@christianspence.co.uk>
2024-02-05 10:58:21 +0000

    Amend unem,parquet to unnested df

--- workbench/parquet-testing.R
+++ workbench/parquet-testing.R
@@ -1,8 +1,15 @@
 edd_datasets$UNEM |>
   edd_obj_to_dataframe() |> 
+  tidyr::unnest() |>
   arrow::write_parquet("workbench/unem.parquet")
 
 unem <- arrow::read_parquet("workbench/unem.parquet",  as_data_frame = FALSE)
 
 str(unem)
 
+unem <- arrow::read_parquet(
+  "https://github.com/economic-analytics/edd/raw/main/workbench/unem.parquet",
+  as_data_frame = FALSE
+)
+
+unem$variable
