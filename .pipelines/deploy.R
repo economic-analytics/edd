a05038670a4690cc4de4a15b2880ddc0ffb19d17
Giles Dring <giles@dringtech.com>
2024-02-08 15:51:08 +0000

    Add deployment pipeline

---
+++ .pipelines/deploy.R
@@ -0,0 +1,26 @@
+# .libPaths("/usr/local/lib/R/site-library")
+
+# use renv to detect and install required packages.
+# if (file.exists("renv.lock")) {
+#   renv::restore(prompt = FALSE)
+# } else {
+#   renv::hydrate()
+# }
+
+# required inputs
+accountName <- Sys.getenv("SHINYAPPS_ACCOUNT")
+accountToken <- Sys.getenv("SHINYAPPS_TOKEN")
+accountSecret <- Sys.getenv("SHINYAPPS_SECRET")
+appDir <- Sys.getenv("GITHUB_WORKSPACE")
+
+# set up account
+cat("checking account info...")
+rsconnect::setAccountInfo(accountName, accountToken, accountSecret)
+cat(" [OK]\n")
+
+# deploy application
+rsconnect::deployApp(
+  appDir = appDir,
+  appName = 'edd-app',
+  account = accountName
+)
