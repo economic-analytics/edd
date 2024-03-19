c098d55a513352e82eb204da856f5402119e2f23
Christian Spence <mail@christianspence.co.uk>
2024-03-19 08:37:33 +0000

    Change position of about tab to last on menubar

--- R/ui.R
+++ R/ui.R
@@ -80,17 +80,22 @@ ui <- function(request) {
     ),
 
     # Menu: about
-    tabPanel(
-      id = "about",
-      title = "About",
-      h2("About EDD"),
-      p("EDD, the Economic Data Dashboard, is a tool currently under development."),
-      p("It is designed to make economic data more useful, particularly at a sub-regional level, to support better analysis and policymaking in local areas."),
-      p("EDD will, in its first beta release (v0.0.1 expected Q2 2024), provide interactive access to most ONS datasets, including small area estimates, providing a repository of small-geography economic data in a structured, linked format, with ISO8601 date formats and standard ONS geography codes to support easier access to cleaned and cubed data from multiple sources."),
-      p("Later, EDD will include Land Registry housing transacation data, all NOMIS datasets and Companies House data for all active companies in the UK, including an API to access all data across multiple sources."),
-      p("The development plan includes both imputation of all economic data to a small-area level (not larger than MSOA) and a quarterly-updated forecasting model for all economies across the UK. Both of these will be on a commercial subscription basis."),
-      p("If you're interested in its development, more information is available on its Github page at github.com/economic-analytics/edd."),
-      p("For more information, please contact Christian Spence at mail@christianspence.co.uk")
+      id = "place_analysis",
+      title = "Place Analysis",
+      titlePanel("EDD: Place Analysis"),
+
+      sidebarLayout(
+        sidebarPanel(
+          p("place_geography"),
+          uiOutput("place_geography"),
+          p("analysis_type"),
+          uiOutput("place_analysis")
+        ),
+        mainPanel(
+          p("place_plot"),
+          plotOutput("place_plot")
+        )
+      )
     ),
 
     tabPanel(
@@ -211,6 +216,20 @@ ui <- function(request) {
       h3("2022-01-08 v0.0.0.9000"),
       p("Initial ALPHA commit"),
       p("Add ONS Regional Gross Fixed Capital Formation")
+    ),
+
+    # Menu: about
+    tabPanel(
+      id = "about",
+      title = "About",
+      h2("About EDD"),
+      p("EDD, the Economic Data Dashboard, is a tool currently under development."),
+      p("It is designed to make economic data more useful, particularly at a sub-regional level, to support better analysis and policymaking in local areas."),
+      p("EDD will, in its first beta release (v0.0.1 expected Q2 2024), provide interactive access to most ONS datasets, including small area estimates, providing a repository of small-geography economic data in a structured, linked format, with ISO8601 date formats and standard ONS geography codes to support easier access to cleaned and cubed data from multiple sources."),
+      p("Later, EDD will include Land Registry housing transacation data, all NOMIS datasets and Companies House data for all active companies in the UK, including an API to access all data across multiple sources."),
+      p("The development plan includes both imputation of all economic data to a small-area level (not larger than MSOA) and a quarterly-updated forecasting model for all economies across the UK. Both of these will be on a commercial subscription basis."),
+      p("If you're interested in its development, more information is available on its Github page at github.com/economic-analytics/edd."),
+      p("For more information, please contact Christian Spence at mail@christianspence.co.uk")
     )
   )
 }
