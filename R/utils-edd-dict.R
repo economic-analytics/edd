6e477765c94bd1649a83d2e90aec78a1faa5898d
Christian Spence <mail@christianspence.co.uk>
2024-04-24 11:07:23 +0100

    Fix bug in not writing back metadata to edd_dict correctly

--- R/utils-edd-dict.R
+++ R/utils-edd-dict.R
@@ -3,7 +3,7 @@ load_edd_dict <- function() {
 }
 
 update_edd_dict <- function(dataset_id, column, data) {
-  edd_dict[[column]][edd_dict$id == dataset_id]  <<- data
+  edd_dict[[column]][edd_dict[["id"]] == dataset_id] <<- data
   readr::write_csv(edd_dict, "data/edd_dict.csv")
 }
 
