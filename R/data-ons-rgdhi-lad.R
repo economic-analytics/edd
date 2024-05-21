758a61dbe06590e814d95045546cc670d3496ad2
Christian Spence <mail@christianspence.co.uk>
2024-05-21 14:44:18 +0100

    Rework RGDHI script

--- R/data-ons-rgdhi-lad.R
+++ R/data-ons-rgdhi-lad.R
@@ -1,68 +1,65 @@
-rgdhi_lad <- function() {
-  rgdhi_lad_url <- "https://www.ons.gov.uk/economy/regionalaccounts/grossdisposablehouseholdincome/datasets/regionalgrossdisposablehouseholdincomelocalauthoritiesbyitl1region"
+ons_process_rgdhi_lad <- function(url = NULL, path = "data-raw") {
 
-  urls <- rvest::read_html(rgdhi_lad_url) |>
-    rvest::html_elements("a") |>
-    rvest::html_attr("href")
+  if (is.null(url)) {
+    url <- "https://www.ons.gov.uk/economy/regionalaccounts/grossdisposablehouseholdincome/datasets/regionalgrossdisposablehouseholdincomelocalauthoritiesbyitl1region"
+  }
 
-  files <- urls[grepl(".xls", urls)]
-  file_urls <- paste0("http://ons.gov.uk", files) # http not https
+  meta <- extract_ons_metadata(url)
 
-  all_data <- lapply(file_urls[1:12], function(file) {
-    local_file_path <- paste0("~/Data/ONS/Regional Accounts/GDHI/",
-                              sub("regionalgrossdisposablehouseholdincome",
-                                  "rgdhi", basename(substring(file, 50))))
-    download.file(file, destfile = local_file_path, mode = "wb")
-    sheets <- readxl::excel_sheets(local_file_path)
-    sheets <- sheets[-(1:2)]
+  local_files <- file.path(path, basename(meta$files))
+
+  # download
+  for (i in meta$files) {
+    download.file(i, file.path(path, basename(i)), mode = "wb")
+    Sys.sleep(2) # avoid rate limiting
+  }
+
+  # process
+  all_data <- lapply(local_files, function(file) {
+    sheets <- readxl::excel_sheets(file)
+    sheets <- sheets[grepl("Table [7-8]", sheets)]
     data <- lapply(sheets, function(sht) {
-      readxl::read_excel(local_file_path, sheet = sht,
-                         skip = 1, na = c("-"))
-    }) |>
-      setNames(c("GDHI current",
-                 "GDHI per capita current",
-                 "GDHI per capita index",
-                 "GDHI annual growth",
-                 "GDHI per capita annual growth",
-                 "GDHI components current",
-                 "GDHI per capita components",
-                 "GDHI per capita components index",
-                 "GDHI components growth",
-                 "GDHI per capita components growth")
+      readxl::read_excel(
+        file,
+        sheet = sht,
+        skip = 1,
+        na = "u",
+        .name_repair = tolower
       )
+    }) |>
+      setNames(
+        c("GDHI total (£m) current prices",
+          "GDHI per capita (£) current prices")
+      ) |>
+      dplyr::bind_rows(.id = "variable.name")
 
     return(data)
-  })
-
-  final <- lapply(all_data, function(x) {
-    lapply(x, function(sht) {
-      if (grepl("Transaction", names(sht)[5])) {
-        c = 1:5
-      } else {
-        c = 1:3
-      }
-
-      dplyr::filter(sht, !is.na(`LAD code`)) |>
-        tidyr::pivot_longer(cols = -(c), names_to = "date")
-    }) |>
-      dplyr::bind_rows(.id = "variable")
   }) |>
     dplyr::bind_rows() |>
-    dplyr::select(date,
-                  geography_code = `LAD code`,
-                  geography_name = `Region name`,
-                  variable_code = variable,
-                  variable_name = variable,
-                  transaction_code = `Transaction code`,
-                  transaction_name = Transaction,
-                  value) |>
-    dplyr::mutate(date = stringr::str_sub(date, 1, 4)) |>
-    tidyr::replace_na(list(transaction_name = "Total")) |>
-    dplyr::mutate(transaction_code = dplyr::coalesce(transaction_code,
-                                                     transaction_name)) |>
-    df_to_edd_df() |>
-    edd_df_to_edd_obj()
+    tidyr::pivot_longer(
+      cols = dplyr::where(is.numeric),
+      names_to = "dates.date"
+    ) |>
+    dplyr::mutate(
+      dataset = "RGDHI",
+      dates.date = as.Date(paste0(dates.date, "-01-01")),
+      dates.freq = "a"
+    ) |>
+    dplyr::select(
+      dataset,
+      dates.date, dates.freq,
+      geography.code = `lad code`,
+      geography.name = `region name`,
+      transaction.code = `transaction code`,
+      transaction.name = transaction,
+      value
+    )
 
-    saveRDS(final, "data/datasets/RGDHI.rds")
-    return(final)
+  # write
+  arrow::write_parquet(all_data, "data/parquet/RGDHI.parquet")
+
+  # update metadata
+  update_edd_dict("RGDHI", "last_update", meta$last_update)
+  update_edd_dict("RGDHI", "next_update", meta$next_update)
+  update_edd_dict("RGDHI", "last_download", Sys.Date())
 }
