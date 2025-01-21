2e628c86be483036370300f2abb68e72683f4c39
ChristianSpence <mail@christianspence.co.uk>
2025-01-21 17:25:28 +0000

    Change dataFileLocation to remote URL and add retrieve_dataset function to read from remote parquet file

--- R/data-load-parquet.R
+++ R/data-load-parquet.R
@@ -1,3 +1,17 @@
-dataFileLocation <- "data/parquet"
+# dataFileLocation <- "data/parquet"
+dataFileLocation <- "https://economic-analytics.co.uk/edd-data/"
 
-edd_datasets <- arrow::open_dataset(dataFileLocation, unify_schemas = TRUE)
+# edd_datasets <- arrow::open_dataset(dataFileLocation, unify_schemas = TRUE)
+# the reading of parquet files will now take place in server.R based on the contents
+# of input$dataset
+
+# new function for retrieving parquet dataset
+retrieve_dataset <- function(dataset_id) {
+  arrow::read_parquet(
+    paste0(
+      dataFileLocation,
+      dataset_id,
+      ".parquet"
+    )
+  )
+}
