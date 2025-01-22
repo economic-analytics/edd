d96a3f45e54735d6c53364881e4e568641dd7162
ChristianSpence <mail@christianspence.co.uk>
2025-01-22 10:47:59 +0000

    Update utils-data-summaries to allow for all variables to show on data catalogue page

--- R/utils-data-summaries.R
+++ R/utils-data-summaries.R
@@ -1,5 +1,13 @@
 show_all_variables <- function() {
-  edd_datasets |>
-    dplyr::distinct(dataset, variable.code, variable.name) |>
-    dplyr::collect()
+  all_dataset_ids <- edd_dict$id[!is.na(edd_dict$last_download)]
+
+  all_variables <- lapply(
+    all_dataset_ids,
+    function(dataset_id) {
+      retrieve_dataset(dataset_id) |>
+        dplyr::distinct(dataset, variable.name)
+    }) |>
+    dplyr::bind_rows()
+
+  return(all_variables)
 }
