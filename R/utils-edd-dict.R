685dd12e33b4d22b38a416df6ab8781ea576304c
ChristianSpence <mail@christianspence.co.uk>
2025-03-21 17:57:38 +0000

    Add update_edd_dict_dates() helper function

--- R/utils-edd-dict.R
+++ R/utils-edd-dict.R
@@ -7,5 +7,17 @@ update_edd_dict <- function(dataset_id, column, data) {
   readr::write_csv(edd_dict, "data/edd_dict.csv")
 }
 
+update_edd_dict_dates <- function(
+  dataset_id,
+  last_update,
+  next_update,
+  last_download = Sys.Date()
+) {
+  edd_dict$last_update[edd_dict$id == dataset_id] <<- last_update
+  edd_dict$next_update[edd_dict$id == dataset_id] <<- next_update
+  edd_dict$last_download[edd_dict$id == dataset_id] <<- last_download
+  readr::write_csv(edd_dict, "data/edd_dict.csv")
+}
+
 # Uses edd_dict as a global variable
 load_edd_dict()
