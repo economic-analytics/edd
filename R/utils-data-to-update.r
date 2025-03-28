9279259bed72c3df8618b436a4db55c43d23846f
Christian Spence <mail@christianspence.co.uk>
2025-03-28 17:08:25 +0000

    Improve handling of changed release dates

--- R/utils-data-to-update.r
+++ R/utils-data-to-update.r
@@ -9,20 +9,49 @@ datasets_to_update <- function() {
 }
 
 check_next_update_dates <- function() {
-  datasets_with_no_next_update <- edd_dict |>
+  d <- edd_dict |>
     dplyr::filter(
       status &
-      is.na(next_update) &
+      provider == "ONS" &
+      (is.na(next_update) | next_update < last_download | next_update < Sys.Date()) &
       !is.na(page_url) &
       grepl("ons.gov.uk", page_url)
     )
 
-  for (id in datasets_with_no_next_update$id) {
-    meta <- extract_ons_metadata(datasets_with_no_next_update$page_url[datasets_with_no_next_update$id == id])
-    Sys.sleep(2)
-    if (!is.na(meta$next_update)) {
-      update_edd_dict(id, "next_update", meta$next_update)
-      message("Successfully found next update date of ", meta$next_update, " for dataset ", id)
+  for (id in d$id) {
+    cat("Looking up", id, "page", "...")
+    tryCatch({
+      meta <- extract_ons_metadata(
+        d$page_url[
+          d$id == id
+        ]
+      )
+      cat("OK\n")
+      # catches if an update has happened but we didn't get it
+      if (meta$last_update > d$last_download[d$id == id]) {
+        update_edd_dict(id, "last_update", meta$last_update)
+        update_edd_dict(id, "next_update", meta$last_update)
+        message(
+          "EDD's version of ", id, " is from ", d$last_download[d$id == id],
+          "\n",
+          "An update of ", id, " occurred on ", meta$last_update,
+          " and we'll get it this time."
+        )
+      } else {
+        if (is.na(meta$next_update) | is.na(d$next_update[d$id == id]) | meta$next_update != d$next_update[d$id == id]) {
+          update_edd_dict(id, "next_update", meta$next_update)
+          message(
+            "Next update date of ", id,
+            " identified for ", meta$next_update
+          )
+        }
+      }
+    },
+    error = function(e) {
+      cat("ERROR with dataset", id, ":", conditionMessage(e), "\n")
     }
+    )
+    # manage rate-limiting
+    Sys.sleep(2)
   }
 }
