7451df0481432dfd28a5702902b54faa2e375cc7
Christian Spence <mail@christianspence.co.uk>
2025-05-09 17:15:38 +0100

    Change `eval()` to `do.call()` and add `dataset_id` argument for calls to `ons_update_datasets()`

--- R/data-ons-update-others.R
+++ R/data-ons-update-others.R
@@ -10,6 +10,15 @@ other_datasets_to_update <- function() {
 
 update_other_datasets <- function() {
   for (dataset in other_datasets_to_update()$id) {
-    eval(call(edd_dict$func[edd_dict$id == dataset]))
+    process_function <- edd_dict$func[edd_dict$id == dataset]
+
+    if (process_function == "ons_update_datasets") {
+      # ons_update_datasets handles more than one base file
+      # so its function needs the id as an argument
+      do.call(process_function, list(dataset_id = dataset))
+    } else {
+      # other process_functions don't yet have the id argument
+      do.call(process_function, list())
+    }
   }
 }
